8758d213e2e36f1b82a4d5ab7f51944396f11f28
Gayu-13 <75153448+Gayu-13@users.noreply.github.com>
2024-03-01 16:03:22 -0600

    Update Gayathri_Plot.R

--- Gayathri_Plot.R
+++ Gayathri_Plot.R
@@ -1,4 +1,53 @@
+# Data_prep
 
+# downloading the required libraries
+library(maps)
+library(gganimate)
+ 
+# Getting coordinate data from the maps library for the world map bg
+World_data <- map_data("world")
+
+# Filtering out lat and long of just the countries in our gapminder dataset
+World_data_gapminder <- world.cities %>% 
+  filter(country.etc %in% gapminder$country)
+
+# The dataset has several coordinate values to exactly map out regions, for our purposes just a rough average location works 
+
+# Finding the average latitude position of each country 
+lat_summarised <- World_data_gapminder %>% 
+   group_by(country.etc) %>%
+   summarise(Avg_lat = mean(lat))
+
+# Finding the average longitude position of each country 
+long_summarised <- World_data_gapminder %>% 
+  group_by(country.etc) %>%
+  summarise(Avg_long = mean(long))
+
+# I realised that there was a naming mismatch which meant I could only extract coordinates for 131/142 countries in gapminder. I am filtering out just those countries for the plot now. 
+gapminder_filtered <- gapminder %>%
+  filter(country %in% lat_summarised$country.etc)
+
+# Matching the country value in each row with the found averages and creating two new columns storing these average latitude and longitude values  
+
+# Initializing dummy vectors
+dummy_lat <- c()
+dummy_long <- c()
+
+for (cy in gapminder_filtered$country){ # go over every country in gapminder
+  dummy_lat <- append(dummy_lat,lat_summarised$Avg_lat[match(cy,lat_summarised$country.etc)]) # match country on the average latitude dataset and add to dummy column
+  dummy_long <- append(dummy_long,long_summarised$Avg_long[match(cy,long_summarised$country.etc)]) # match country on the average longitude dataset and add to dummy column
+}
+
+# Adding those coordinate_matched new columns to our dataset using mutate
+gapminder_mapped <- gapminder_filtered %>%
+  mutate(latitude = dummy_lat) %>%
+  mutate(longitude = dummy_long)
+
+# viewing the final dataset
+head(gapminder_mapped)
+
+
+# Plotting 
 #Doesn't play
 library(viridis)
 library(plotly)
